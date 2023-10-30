8f3d81dfc3f4734c212c1aa27766b20ee840a5c5
Kulpot <kulpotwahu@gmail.com>
2023-10-30 14:40:22 +0800

    https://www.youtube.com/watch?v=LnJ8HgsfZow&list=PLRwVmtr-pp05c1HTBj1no6Fl6C6mlxYDG&index=32

--- Alternating Operations - Our Next Challenge/Mainer.cpp
+++ Alternating Operations - Our Next Challenge/Mainer.cpp
@@ -1,5 +1,5 @@
 
-//ref link:
+//ref link:https://www.youtube.com/watch?v=LnJ8HgsfZow&list=PLRwVmtr-pp05c1HTBj1no6Fl6C6mlxYDG&index=32
 //
 
 // MyAssembly.asm - (customBuildTool)commandline - ml /c /Cx /coff "%(FullPath)"
